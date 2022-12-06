93cbaea904b9008a1f66be9ad4dad874764c62a8
Elliton <ellitondias322@gmail.com>
2022-12-06 01:00:40 -0300

    update

--- app/src/test/java/com/ufc/academiaufc/UsuarioUnitTest.java
+++ app/src/test/java/com/ufc/academiaufc/UsuarioUnitTest.java
@@ -1,8 +1,6 @@
 package com.ufc.academiaufc;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
